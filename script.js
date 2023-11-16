67422d9288502a5a8961f77e8e7edb6d1c4f5059
Dallas <S10258596@connect.np.edu.sg>
2023-11-16 20:39:06 +0800

    Fixed Logo, Heading and added Text. Improved Nav.

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 let currentIndex = 0;
 const images = document.querySelectorAll("#slider .image");
-const texts = document.querySelectorAll("#slider #text p");
+const textContents = document.querySelectorAll("#slider .text-content"); // Select the .text-content divs
 const totalImages = images.length;
 
-document.getElementById("prev").addEventListener("click", () => changeImage(-1));
-document.getElementById("next").addEventListener("click", () => changeImage(1));
+document.getElementById("prev").addEventListener("click", () => changeSlide(-1));
+document.getElementById("next").addEventListener("click", () => changeSlide(1));
 
-function changeImage(step) {
-    images[currentIndex].hidden = true;
-    texts[currentIndex].hidden = true;
+function changeSlide(step) {
+    // Hide the currently displayed image and text
+    images[currentIndex].style.display = 'none';
+    textContents[currentIndex].classList.remove('active'); // Adjust to target .text-content div
+
+    // Update the current index
     currentIndex = (currentIndex + step + totalImages) % totalImages;
-    images[currentIndex].hidden = false;
-    texts[currentIndex].hidden = false;
+
+    // Show the new image and text
+    images[currentIndex].style.display = 'block';
+    textContents[currentIndex].classList.add('active'); // Adjust to target .text-content div
 }
 
-setInterval(() => changeImage(1), 1000); // Change image every 3 seconds
+// Initialize first slide and text
+changeSlide(0);
+
+// Optional: Automatic slide change every 3 seconds
+setInterval(() => changeSlide(1), 3000);
