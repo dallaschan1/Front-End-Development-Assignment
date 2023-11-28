32a8090760510a553e404f09275ed471ff8d7975
Dallas <S10258596@connect.np.edu.sg>
2023-11-28 20:20:28 +0800

    finished Menu Page

---
+++ menu-script.js
@@ -0,0 +1,16 @@
+function updateMenu(category) {
+    // Hide all menu categories
+    document.querySelectorAll('.menu-selector').forEach(function(menuItem) {
+        menuItem.classList.remove('active');
+    });
+    document.querySelectorAll('.menu-content').forEach(menu => {
+        menu.style.display = 'none';
+    });
+    document.querySelector('.menu-selector[onclick="updateMenu(\'' + category + '\')"]').classList.add('active');
+    // Show the selected category
+    document.getElementById(category + '-menu').style.display = 'block';
+}
+
+document.getElementById('viewMenuButton').addEventListener('click', function() {
+    document.getElementById('content').scrollIntoView({ behavior: 'smooth' });
+});
--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 let currentIndex = 0;
 const images = document.querySelectorAll("#slider .image");
-const textContents = document.querySelectorAll("#slider .text-content"); // Select the .text-content divs
-const totalImages = images.length;
+const textContents = document.querySelectorAll("#slider .text-content");
 
 document.getElementById("prev").addEventListener("click", () => changeSlide(-1));
 document.getElementById("next").addEventListener("click", () => changeSlide(1));
 
 function changeSlide(step) {
-    // Hide the currently displayed image and text with fade out
-    images[currentIndex].classList.remove('active');
-    textContents[currentIndex].classList.remove('active');
-
-    // Update the current index
-    currentIndex = (currentIndex + step + totalImages) % totalImages;
-
-    // Show the new image and text with fade in
-    images[currentIndex].classList.add('active');
-    textContents[currentIndex].classList.add('active');
+    images[currentIndex].classList.add('zooming');
+    setTimeout(() => {
+        const nextIndex = (currentIndex + step + totalImages) % totalImages;
+        images[nextIndex].classList.add('active');
+        textContents[nextIndex].classList.add('active');
+        images[currentIndex].classList.remove('active', 'zooming');
+        textContents[currentIndex].classList.remove('active');
+        currentIndex = nextIndex;
+    }, 3000); // Zoom duration
 }
 
-
-// Initialize first slide and text
 changeSlide(0);
-
-// Optional: Automatic slide change every 3 seconds
 setInterval(() => changeSlide(1), 3000);
